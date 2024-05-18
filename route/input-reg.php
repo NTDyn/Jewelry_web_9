2fe81384ead80db46e9922e06a5f1fb5ff1ab96f
Anhdeptroai <nguyentrunganh30092003@gmail.com>
2024-05-18 08:06:43 +0700

    Fix phone number

--- route/input-reg.php
+++ route/input-reg.php
@@ -33,34 +33,41 @@ if(isset($_POST['register'])){
             //echo "<pre>";
             //Xuất thông tin tại cái ô input
             //print_r($_POST);
-        if (isValidPhoneNumber($phone)) {           
-            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {    
-                $sql1 = "SELECT * FROM customer WHERE Customer_Username = '$username'";
-                $result = $conn->query($sql1);
-                if ($result->num_rows == 0) {
-                    if($password != $com_password){
-                        echo "<script>alert('Mật khẩu nhập lại không trùng khớp')</script>";	
-                        echo "<script>window.location.href = '../views/user/register.php';</script>";	
+        if (isValidPhoneNumber($phone)) {     
+            $sql10 = "SELECT * FROM customer WHERE Customer_Phone = '$phone'";
+                $result10 = $conn->query($sql10);
+                if ($result10->num_rows == 0) {
+                    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {    
+                        $sql1 = "SELECT * FROM customer WHERE Customer_Username = '$username'";
+                        $result = $conn->query($sql1);
+                        if ($result->num_rows == 0) {
+                            if($password != $com_password){
+                                echo "<script>alert('Mật khẩu nhập lại không trùng khớp')</script>";	
+                                echo "<script>window.location.href = '../views/user/register.php';</script>";	
+                            }else{
+                                $sql = "INSERT INTO `customer` (`Customer_Name`, `Customer_Phone`, `Customer_Address`, `Customer_Email`, `Customer_Username`,`Customer_Password`, `Customer_Status`)
+                                        VALUES('$fullname', '$phone', '$address', '$email', '$username', '$password', 1)";// mã hóa md5('$password')
+                                
+                                if($conn->query($sql) === TRUE){
+                                    $content = "Đăng ký thành công";
+                                    echo "<script>alert('Đăng ký thành công')</script>";
+                                    echo "<script>window.location.href = '../views/user/login.php';</script>";
+                                }else{
+                                    echo "Lỗi {$sql}".$conn->error;
+                                }
+                            }    
+                        } else {
+                            echo "<script>alert('Username đã tồn tại')</script>";	
+                            echo "<script>window.location.href = '../views/user/register.php';</script>";
+                        }                             
                     }else{
-                        $sql = "INSERT INTO `customer` (`Customer_Name`, `Customer_Phone`, `Customer_Address`, `Customer_Email`, `Customer_Username`,`Customer_Password`, `Customer_Status`)
-                                VALUES('$fullname', '$phone', '$address', '$email', '$username', '$password', 1)";// mã hóa md5('$password')
-                        
-                        if($conn->query($sql) === TRUE){
-                            $content = "Đăng ký thành công";
-                            echo "<script>alert('Đăng ký thành công')</script>";
-                            echo "<script>window.location.href = '../views/user/login.php';</script>";
-                        }else{
-                            echo "Lỗi {$sql}".$conn->error;
-                        }
-                    }    
-                } else {
-                    echo "<script>alert('Username đã tồn tại')</script>";	
+                        echo "<script>alert('Email không hợp lệ')</script>";
+                        echo "<script>window.location.href = '../views/user/register.php';</script>";
+                    }
+                }else{    
+                    echo "<script>alert('Số điện thoại bạn đăng kí đã bị trùng')</script>";
                     echo "<script>window.location.href = '../views/user/register.php';</script>";
-                }                             
-            }else{
-                echo "<script>alert('Email không hợp lệ')</script>";
-                echo "<script>window.location.href = '../views/user/register.php';</script>";
-            }
+                }
         }else{
             echo "<script>alert('Số điện thoại không hợp lệ')</script>";
             echo "<script>window.location.href = '../views/user/register.php';</script>";
